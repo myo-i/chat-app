4218fef250054cc62af09b4ca98453f3d56cdfd8
myo-i <Imiyo0521@gmail.com>
2020-09-07 23:07:44 +0900

    messagesテーブルの作成+routing修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "room#index"
+  
+  root to: "rooms#index"
   resources :users,  only: [:edit, :update]
   resources :rooms,  only: [:new, :create]
   get 'messages/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "messages#index"
+  
 end
