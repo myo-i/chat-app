b653d0fa1387fc7304f6a45033f3b0cec340b453
myo-i <Imiyo0521@gmail.com>
2020-09-07 13:54:15 +0900

    画面遷移の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  root to: "room#index"
   resources :users,  only: [:edit, :update]
   resources :rooms,  only: [:new, :create]
   get 'messages/index'
